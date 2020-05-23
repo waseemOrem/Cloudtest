de332c426714b5788ac61f25a2c3233c742da452
waseem Malik <waseem.orem@gmail.com>
2020-05-23 08:00:29 +0530

    Initial Commit

---
+++ Cloudtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cloudtest
+//
+//  Created by admin on 23/05/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
